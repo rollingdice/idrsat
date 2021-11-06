b12b912362ea586f320a929c00f034f94370cced
Banitama <banitama.supartha@gmail.com>
2021-11-06 14:20:42 +0100

    Add filehandler
    Add filehandler for CSS and JS files. The files should be presented
    in /static subfolder.

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
-import requests
 import tornado.ioloop
 import tornado.web
 import tornado.httpclient
 import tornado.gen
 import math
 import json
+import os.path
+from mimetypes import guess_type
 
 idrpersat = 10
 
+BASEDIR_NAME = os.path.dirname(__file__)
+BASEDIR_PATH = os.path.abspath(BASEDIR_NAME)
+
+FILES_ROOT = os.path.join(BASEDIR_PATH, 'static')
+
 class MainHandler(tornado.web.RequestHandler):
     def set_default_headers(self):
-        #print "setting headers!!!"
         self.set_header("Access-Control-Allow-Origin", "*")
         self.set_header("Access-Control-Allow-Headers", "x-requested-with")
         self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
@@ -18,9 +23,20 @@ class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write(str(idrpersat))
 
+class FileHandler(tornado.web.RequestHandler):
+    def get(self, path):
+        file_location = os.path.join(FILES_ROOT, path)
+        if not os.path.isfile(file_location):
+            raise tornado.web.HTTPError(status_code=404)
+        content_type, _ = guess_type(file_location)
+        self.add_header('Content-Type', content_type)
+        with open(file_location) as source_file:
+            self.write(source_file.read())
+
 def make_app():
     return tornado.web.Application([
         (r"/idrsat", MainHandler),
+        tornado.web.url(r"/static/(.+)", FileHandler),
     ])
 
 async def refresh_price():
